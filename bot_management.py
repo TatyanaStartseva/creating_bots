003af80c904d5599b76cfa4ac4ee13d5abc01469
Startseva Tatyana <TatyanaStartseva2020@yandex.ru>
2024-05-10 16:41:11 +0300

    удалены прямые пути к директориям

--- bot_management.py
+++ bot_management.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from aiohttp import web
-from startup import stop_processing, directory_path
+from pm2_bots_manager import stop_processing, directory_path
 import os
 import requests
 import git
@@ -8,41 +8,85 @@ import shutil
 import json
 import uuid
 import aiofiles
-load_dotenv()
-REMOVE_SENT_CONFIRMATION=os.getenv("REMOVE_SENT_CONFIRMATION")
-DATABASE_URI=os.getenv("DATABASE_URI")
-DATABASE_NAME=os.getenv("DATABASE_NAME")
-DATABASE_COLLECTION=os.getenv("DATABASE_COLLECTION")
-path = "C:\\Users\\User\\PycharmProjects\\pythonProject6\\mega_bot\\web_app\\bots"
-path_video ="C:\\Users\\User\\PycharmProjects\\pythonProject6\\mega_bot\\web_app\\video"
-repo_url="https://github.com/TatyanaStartseva/mega_bot"
 
+load_dotenv()
+REMOVE_SENT_CONFIRMATION = os.getenv("REMOVE_SENT_CONFIRMATION")
+DATABASE_URI = os.getenv("DATABASE_URI")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_COLLECTION = os.getenv("DATABASE_COLLECTION")
+repo_url = "https://github.com/TatyanaStartseva/mega_bot"
+
+
+def get_bot_directory():
+    directory = os.path.abspath(os.path.join(os.path.dirname(__file__), "bots"))
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return directory
+
+
+def get_video_directory():
+    directory = os.path.abspath(os.path.join(os.path.dirname(__file__), "video"))
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return directory
 
 
 async def clone_repo(bot_token, data):
     try:
         bot_token_str = str(bot_token).replace("!", ":")
-        clone_path = f"{path}\\{bot_token}"
-        data['bot_token'] = bot_token_str
+        clone_path = os.path.join(get_bot_directory(), bot_token)
+        data["bot_token"] = bot_token_str
         if not os.path.exists(clone_path):
             os.makedirs(clone_path)
         git.Repo.clone_from(repo_url, clone_path)
-        with open (f"{clone_path}\\setup.json","w") as f:
-            json.dump(data,f)
-        with open (f"{clone_path}\\.env","w") as f:
-            f.write(f"REMOVE_SENT_CONFIRMATION = {REMOVE_SENT_CONFIRMATION}\n DATABASE_URI ={DATABASE_URI}\n DATABASE_NAME= {DATABASE_NAME}\n DATABASE_COLLECTION= {str(bot_token_str)}\n")
+        with open(f"{clone_path}\\setup.json", "w") as f:
+            json.dump(data, f)
+        with open(f"{clone_path}\\.env", "w") as f:
+            f.write(
+                f"REMOVE_SENT_CONFIRMATION = {REMOVE_SENT_CONFIRMATION}\n DATABASE_URI ={DATABASE_URI}\n DATABASE_NAME= {DATABASE_NAME}\n DATABASE_COLLECTION= {str(bot_token_str)}\n"
+            )
     except Exception as e:
         print(f"{e}")
         return web.Response(text=f"{e}")
 
+
 async def update_bot(requests):
     try:
         data = await requests.json()
-        bot_token = requests.match_info['id']
-        data['bot_token'] = str(bot_token).replace("!", ":")
+        bot_token = requests.match_info["id"]
+        required_keys = ["admin_chat_id", "name", "steps"]
+        for key in required_keys:
+            if key not in data:
+                return web.Response(text=f"Отсутствует ключ '{key}' в данных")
+
+        if not isinstance(data["admin_chat_id"], int):
+            return web.Response(text="Значение 'admin_chat_id' должно быть числом")
+
+        if not isinstance(data["name"], str):
+            return web.Response(text="Значение 'name' должно быть строкой")
+
+        if not isinstance(data["steps"], list):
+            return web.Response(text="Значение 'steps' должно быть списком")
+
+        for step in data["steps"]:
+            if not isinstance(step, dict):
+                return web.Response(
+                    text="Элементы списка 'steps' должны быть словарями"
+                )
+            if "type" not in step:
+                return web.Response(
+                    text="Каждый элемент в списке 'steps' должен содержать ключ 'type'"
+                )
+            allowed_types = ["text", "video", "website", "zoomcall"]
+            if not any(step_type in allowed_types for step_type in step["type"]):
+                return web.Response(
+                    text="Значение ключа 'type' должно быть одним из: 'text', 'video', 'website', 'zoomcall'"
+                )
+
+        data["bot_token"] = str(bot_token).replace("!", ":")
         bot_number, bot_token = bot_token.split(":")
         bot_token_str = f"{bot_number}!{bot_token}"
-        exist_bot_path = f"{path}\\{bot_token_str}"
+        exist_bot_path = os.path.join(get_bot_directory(), bot_token_str)
         if os.path.exists(exist_bot_path):
             setup_json_path = os.path.join(exist_bot_path, "setup.json")
             if os.path.exists(setup_json_path):
@@ -53,7 +97,9 @@ async def update_bot(requests):
                 with open(f"{setup_json_path}", "w") as f:
                     json.dump(data, f)
         else:
-            return web.Response(text=f"Бот с токеном {exist_bot_path} не найден. Добавьте бота.")
+            return web.Response(
+                text=f"Бот с токеном {exist_bot_path} не найден. Добавьте бота."
+            )
         return web.Response(text="Обновление завершено")
     except Exception as e:
         print(f"{e}")
@@ -62,16 +108,16 @@ async def update_bot(requests):
 
 async def bot_info(requests):
     try:
-        bot_token = requests.match_info['id']
+        bot_token = requests.match_info["id"]
         bot_token_str = str(bot_token)
         bot_number, bot_token = bot_token.split(":")
         bot_token_str = f"{bot_number}!{bot_token}"
-        exist_bot_path = f"{path}\\{bot_token_str}"
+        exist_bot_path = os.path.join(get_bot_directory(), bot_token_str)
         if os.path.exists(exist_bot_path):
             setup_json_path = os.path.join(exist_bot_path, "setup.json")
             with open(setup_json_path, "r") as file:
-                    setup_data = json.load(file)
-                    return web.json_response(setup_data)
+                setup_data = json.load(file)
+                return web.json_response(setup_data)
         else:
             return web.Response(text="Бот не найден")
     except Exception as e:
@@ -81,21 +127,21 @@ async def bot_info(requests):
 
 async def delete_bot(requests):
     try:
-        bot_token = requests.match_info['id']
+        bot_token = requests.match_info["id"]
         bot_token_str = str(bot_token)
         bot_number, bot_token = bot_token.split(":")
         bot_token_str = f"{bot_number}!{bot_token}"
         stop_processing(bot_token_str)
-        exist_bot_path = f"{path}\\{bot_token_str}"
+        exist_bot_path = os.path.join(get_bot_directory(), bot_token_str)
         git_dir = os.path.join(exist_bot_path, ".git")
         os.system(f'attrib -h "{git_dir}"')
         files_list = os.listdir(f"{git_dir}\\objects\\pack\\")
         if os.path.exists(git_dir):
             for file_name in files_list:
-                if file_name.endswith('.idx'):
+                if file_name.endswith(".idx"):
                     file_path = os.path.join(f"{git_dir}\\objects\\pack\\", file_name)
                     os.chmod(file_path, 0o777)
-                if file_name.endswith('.pack'):
+                if file_name.endswith(".pack"):
                     file_path = os.path.join(f"{git_dir}\\objects\\pack\\", file_name)
                     os.chmod(file_path, 0o777)
             shutil.rmtree(git_dir)
@@ -107,85 +153,106 @@ async def delete_bot(requests):
     except Exception as e:
         print(f"{e}")
         return web.Response(text=f"{e}")
+
+
 async def create_bot(requests):
     try:
-        bot_token = requests.match_info['id']
+        bot_token = requests.match_info["id"]
         data = await requests.json()
 
-        required_keys = ['admin_chat_id', 'name', 'steps']
+        required_keys = ["admin_chat_id", "name", "steps"]
         for key in required_keys:
             if key not in data:
                 return web.Response(text=f"Отсутствует ключ '{key}' в данных")
 
-        if not isinstance(data['admin_chat_id'], int):
+        if not isinstance(data["admin_chat_id"], int):
             return web.Response(text="Значение 'admin_chat_id' должно быть числом")
 
-        if not isinstance(data['name'], str):
+        if not isinstance(data["name"], str):
             return web.Response(text="Значение 'name' должно быть строкой")
 
-        if not isinstance(data['steps'], list):
+        if not isinstance(data["steps"], list):
             return web.Response(text="Значение 'steps' должно быть списком")
 
-        for step in data['steps']:
+        for step in data["steps"]:
             if not isinstance(step, dict):
-                return web.Response(text="Элементы списка 'steps' должны быть словарями")
-            if 'type' not in step:
-                return web.Response(text="Каждый элемент в списке 'steps' должен содержать ключ 'type'")
-            allowed_types = ['text', 'video', 'website', 'zoomcall']
-            if step['type'] not in allowed_types:
                 return web.Response(
-                    text="Значение ключа 'type' должно быть одним из: 'text', 'video', 'website', 'zoomcall'")
+                    text="Элементы списка 'steps' должны быть словарями"
+                )
+            if "type" not in step:
+                return web.Response(
+                    text="Каждый элемент в списке 'steps' должен содержать ключ 'type'"
+                )
+            allowed_types = ["text", "video", "website", "zoomcall"]
+            if not any(step_type in allowed_types for step_type in step["type"]):
+                return web.Response(
+                    text="Значение ключа 'type' должно быть одним из: 'text', 'video', 'website', 'zoomcall'"
+                )
 
-        bot_token = requests.match_info['id']
+        bot_token = requests.match_info["id"]
         data = await requests.json()
         bot_token_str = str(bot_token)
         bot_number, bot_token = bot_token.split(":")
         bot_token_str = f"{bot_number}!{bot_token}"
-        exist_bot_path = f"{path}\\{bot_token_str}"
+        exist_bot_path = os.path.join(get_bot_directory(), bot_token_str)
         if not os.path.exists(exist_bot_path):
-            await clone_repo(bot_token_str,data)
+            await clone_repo(bot_token_str, data)
             return web.Response(text="Бот создан")
         else:
-            return web.Response(text=f"Этот бот уже существует {bot_token}, папка должн называться {bot_token_str}")
+            return web.Response(
+                text=f"Этот бот уже существует {bot_token}, папка должн называться {bot_token_str}"
+            )
     except Exception as e:
         print(f"{e}")
         return web.Response(text=f"{e}")
 
 
-
 async def get_video_path(requests):
     try:
-        video_name = requests.query.get('video_name')
-        video_path = os.path.join(path_video, video_name + '.mp4')
+        video_name = requests.query.get("video_name")
+        path_video = get_video_directory()
+        video_path = os.path.join(path_video, video_name + ".mp4")
         if os.path.exists(video_path):
             response = web.FileResponse(video_path)
-            response.headers['Content-Type'] = 'video/mp4'
+            response.headers["Content-Type"] = "video/mp4"
             return response
         else:
             return web.json_response({"error": "Этого файла нет"})
     except Exception as e:
-        return web.json_response({"error": f"Произошла ошибка при загрузке видео: {str(e)}"})
+        return web.json_response(
+            {"error": f"Произошла ошибка при загрузке видео: {str(e)}"}
+        )
 
 
 async def upload_video(request):
     try:
         data = await request.post()
-        video = data['video'].file
+        video = data["video"].file
         filename = str(uuid.uuid4())
-        filepath = path_video +'\\' + filename + '.mp4'
-        async with aiofiles.open(filepath, 'wb') as f:
+        path_video = get_video_directory()
+        filepath = path_video + "\\" + filename + ".mp4"
+        async with aiofiles.open(filepath, "wb") as f:
             while True:
                 chunk = video.read(1024)
                 if not chunk:
                     break
                 await f.write(chunk)
         return web.json_response(
-            {"uuid": f"{filename}", "message": "Видео успешно скачено. Его название соответвует ключу uuid"})
+            {
+                "uuid": f"{filename}",
+                "message": "Видео успешно скачено. Его название соответвует ключу uuid",
+            }
+        )
     except Exception as e:
-        return web.json_response({"error": f"Произошла ошибка при загрузке видео: {str(e)}"})
+        return web.json_response(
+            {"error": f"Произошла ошибка при загрузке видео: {str(e)}"}
+        )
+
+
 async def bots_info(requests):
     try:
         dirs_list = []
+        path = get_bot_directory()
         for dir in next(os.walk(path))[1]:
             exist_bot_path = f"{path}\\{dir}"
             if os.path.exists(exist_bot_path):
@@ -198,15 +265,13 @@ async def bots_info(requests):
         print(f"{e}")
         return web.Response(text=f"{e}")
 
+
 app = web.Application(client_max_size=1024**3)
-app.router.add_get('/get_video_path', get_video_path)
+app.router.add_get("/get_video_path", get_video_path)
 app.router.add_get("/bots/{id}", bot_info)
 app.router.add_get("/bots", bots_info)
 app.router.add_post("/bots/{id}", create_bot)
 app.router.add_patch("/bots/{id}", update_bot)
 app.router.add_delete("/bots/{id}", delete_bot)
-app.router.add_post('/upload', upload_video)
+app.router.add_post("/upload", upload_video)
 web.run_app(app, port=80)
-
-
-
--- pm2_bots_manager.py
+++ pm2_bots_manager.py
@@ -1,105 +1,102 @@
 import os
 import subprocess
 import time
-python_path = "C:\\Users\\User\\PycharmProjects\\pythonProject6\\Scripts\\python.exe"
-directory_path = 'C:/Users/User/PycharmProjects/pythonProject6/mega_bot/web_app/bots'
-pm2_list_file = 'pm2_processes.txt'
+import re
+
+
+def get_bot_directory():
+    directory = os.path.abspath(os.path.join(os.path.dirname(__file__), "bots"))
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return directory
+
 
 def stop_processing(bot_token):
-    process = subprocess.Popen(['pm2', 'ls'], stdout=subprocess.PIPE, shell=True)
+    process = subprocess.Popen(["pm2", "ls"], stdout=subprocess.PIPE, shell=True)
     output, _ = process.communicate()
-    output_str = output.decode('utf-8')
+    output_str = output.decode("utf-8")
     if bot_token in output_str:
-        folder_full_path = os.path.join(directory_path, bot_token)
-        pm2_command = f'pm2 stop {bot_token}'
-        subprocess.run(pm2_command, shell=True, cwd=folder_full_path, encoding='utf-8')
+        folder_full_path = os.path.join(get_bot_directory(), bot_token)
+        pm2_command = f"pm2 stop {bot_token}"
+        subprocess.run(pm2_command, shell=True, cwd=folder_full_path, encoding="utf-8")
     return
 
 
-def get_folders_in_directory(directory):
-    return [folder for folder in os.listdir(directory) if os.path.isdir(os.path.join(directory, folder))]
-
-
-def get_pm2_processes(directory):
-    process = subprocess.Popen(['pm2', 'ls'], stdout=subprocess.PIPE, shell=True)
+def get_pm2_processes():
+    process = subprocess.Popen(["pm2", "ls"], stdout=subprocess.PIPE, shell=True)
     output, _ = process.communicate()
-    output_str = output.decode('utf-8')
+    output_str = output.decode("utf-8")
+    lines = output_str.split("\n")
+    process_names = []
+    for line in lines[3:]:
+        parts = line.split()
+        if len(parts) > 1:
+            process_name = parts[3]
+            process_names.append(process_name)
 
-    folders = get_folders_in_directory(directory)
-    pm2_processes = []
-
-    for folder in folders:
-        if folder in output_str:
-            pm2_processes.append(folder)
-
-    return pm2_processes
-
-def write_pm2_processes_to_file(processes):
-    with open(pm2_list_file, 'w', encoding='utf-8', errors='ignore') as file:
-        for process in processes:
-            file.write(process + '\n')
-
-
-def read_pm2_processes_from_file():
-    if os.path.exists(pm2_list_file):
-        with open(pm2_list_file, 'r', encoding='utf-8') as file:
-            return file.read().splitlines()
-    return []
+    return process_names
 
 
 def add_folders_to_pm2(added_folders, directory):
     try:
-        process = subprocess.Popen(['pm2', 'ls'], stdout=subprocess.PIPE, shell=True)
+        process = subprocess.Popen(["pm2", "ls"], stdout=subprocess.PIPE, shell=True)
         output, _ = process.communicate()
-        output_str = output.decode('utf-8')
+        output_str = output.decode("utf-8")
         if added_folders:
-                for folder in added_folders:
-                    if folder not in output_str:
-                        folder_full_path = os.path.join(directory, folder)
-                        python_path = "C:\\Users\\User\\PycharmProjects\\pythonProject6\\Scripts\\python.exe"
-                        pm2_command = f'pm2 start {python_path} --name {folder} --watch -- -m bot'
-                        time.sleep(30)
-                        subprocess.run(pm2_command, shell=True, cwd=folder_full_path, encoding='utf-8')
-                    else:
-                        print("Нет новых папок для добавления в pm2.")
+            for folder in added_folders:
+                if folder not in output_str:
+                    folder_full_path = os.path.join(directory, folder)
+                    pm2_command = f"pm2 start python --name {folder} --watch -- -m bot"
+                    time.sleep(30)
+                    subprocess.run(
+                        pm2_command, shell=True, cwd=folder_full_path, encoding="utf-8"
+                    )
+                else:
+                    print("Нет новых папок для добавления в pm2.")
         else:
             print("Нет новых папок для добавления в pm2.")
     except subprocess.CalledProcessError as e:
-            print(f"Ошибка при запуске процесса: {e}")
+        print(f"Ошибка при запуске процесса: {e}")
+
 
 def delete_folders_from_pm2(removed_folders, directory):
     try:
-        process = subprocess.Popen(['pm2', 'ls'], stdout=subprocess.PIPE, shell=True)
+        process = subprocess.Popen(["pm2", "ls"], stdout=subprocess.PIPE, shell=True)
         output, _ = process.communicate()
-        output_str = output.decode('utf-8')
+        output_str = output.decode("utf-8")
         if removed_folders:
-                for folder in removed_folders:
-                    if folder in output_str:
-                        pm2_command = f'pm2 delete {folder}'
-                        subprocess.run(pm2_command, shell=True, cwd=directory, encoding='utf-8')
+            for folder in removed_folders:
+                if folder in output_str:
+                    pm2_command = f"pm2 delete {folder}"
+                    subprocess.run(
+                        pm2_command, shell=True, cwd=directory, encoding="utf-8"
+                    )
         else:
             print("Нет папок для удаления из pm2.")
     except Exception as e:
         print(f"Ошибка выполнения команды: ")
         print(f"{e}")
 
+
 def monitor_folder(directory):
     while True:
-        current_processes = get_pm2_processes(directory)
-        previous_processes = read_pm2_processes_from_file()
-        added_folders = [folder for folder in os.listdir(directory) if folder not in previous_processes]
-        removed_folders = [folder for folder in previous_processes if folder not in os.listdir(directory)]
+        current_processes = get_pm2_processes()
+        added_folders = [
+            folder
+            for folder in os.listdir(directory)
+            if folder not in current_processes
+        ]
+        removed_folders = [
+            folder
+            for folder in current_processes
+            if folder not in os.listdir(directory)
+        ]
         if added_folders:
             add_folders_to_pm2(added_folders, directory)
-            current_processes += added_folders
-
         if removed_folders:
             delete_folders_from_pm2(removed_folders, directory)
-            current_processes = [process for process in current_processes if process not in removed_folders]
-
-        write_pm2_processes_to_file(current_processes)
-
         time.sleep(5)
 
+
 if __name__ == "__main__":
-    monitor_folder(directory_path)
+    monitor_folder(get_bot_directory())
